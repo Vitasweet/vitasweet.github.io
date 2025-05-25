c02964ae646ecd0953ba3d2d853a95aed598223a
Vitasweet <vitahitrenko1986@gmail.com>
2025-05-25 21:44:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function setLanguage(lang) {
+  document.querySelectorAll('[data-ua], [data-en]').forEach(el => {
+    el.textContent = el.dataset[lang];
+  });
+}
+
+function sendMessage() {
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const message = document.getElementById('message').value.trim();
+  const text = `Привіт! Мене звати ${name}. Я хочу замовити: ${message}. Мій email: ${email}`;
+  const encodedText = encodeURIComponent(text);
+  document.getElementById('whatsapp-link').href = `https://wa.me/14372441529?text=${encodedText}`;
+  document.getElementById('instagram-link').href = `https://instagram.com/vitas_weet`;
+}
